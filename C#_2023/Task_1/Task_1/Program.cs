6880fcb20d5da40cea5ecc55aa39383880705701
Denis Romanovskiy <53230856+Skillpolice@users.noreply.github.com>
2023-08-15 12:57:40 +0300

    Add files via upload

---
+++ C#_2023/Task_1/Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int _age = 30;
+
+            Console.WriteLine("Age = " + _age);
+            Console.ReadKey();
+
+        }
+    }
+}
